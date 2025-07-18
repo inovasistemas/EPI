681ea26e924502017f30ed1b50d93ea09bb50330
João <contato@joaofelipegomes.com>
2025-07-18 09:04:42 -0300

    navlink data-active

--- components/Navigation/NavLink/index.tsx
+++ components/Navigation/NavLink/index.tsx
@@ -7,16 +7,15 @@ type NavLinkProps = {
   name?: string
   icon: React.ReactElement
   href: string
-  action?: () => void
 }
 
-export function NavLink({ name, icon, href, action }: NavLinkProps) {
+export function NavLink({ name, icon, href }: NavLinkProps) {
   const pathName = usePathname()
 
   return (
     <Link
       href={href}
-      data-active={pathName === href}
+      data-active={pathName.includes(href)}
       className='group flex items-center gap-1 data-[active=true]:bg-[--backgroundPrimary] hover:bg-[--backgroundPrimary] mt-1.5 py-1 data-[active=false]:py-2 data-[active=true]:py-2 rounded-lg font-normal active:scale-95 transition-all duration-300 data-[active=true]'
     >
       <div className='group flex justify-center min-w-[32px] !max-w-[32px]'>
