e7623ffd5fe29e68cd40648c85d5afa89bd9864a
João <contato@joaofelipegomes.com>
2025-07-31 13:54:15 -0300

    issue small chart

--- app/painel/page.tsx
+++ app/painel/page.tsx
@@ -1,4 +1,8 @@
 'use client'
+
+import { SmallChartIssues } from '@/components/Chart/Small/Issues'
+import { CaretUpIcon } from '@/components/Display/Icons/CaretUp'
+
 export default function Home() {
   return (
     <div className='flex flex-col gap-6 bg-[--backgroundSecondary] sm:pr-3 pb-8 sm:pb-3 w-full lg:h-[calc(100vh-50px)] overflow-auto'>
@@ -21,6 +25,40 @@ export default function Home() {
             clique para ver a lista
           </span>
         </button> */}
+
+        <div className='gap-3 grid sm:grid-cols-3'>
+          <div className=''></div>
+          <div className=''></div>
+          <div className='group relative flex flex-col justify-center items-start p-2 py-3 rounded-2xl w-full transition-all duration-300'>
+            <div className='flex justify-between items-center px-1 w-full'>
+              <h3 className='font-medium text-base'>Taxa de pendências</h3>
+              <span className='rotate-90'>
+                <CaretUpIcon size='size-5' stroke='stroke-[--textSecondary]' />
+              </span>
+            </div>
+            <SmallChartIssues />
+            <div className='flex flex-col gap-4 px-1 w-full'>
+              <div className='flex justify-between items-center pr-1 w-full h-5'>
+                <div className='flex items-center gap-2 h-full'>
+                  <span className='bg-[--primaryColor] rounded-full w-1 h-full'></span>
+                  <span className='font-medium leading-none'>78,81%</span>
+                </div>
+                <span className='opacity-50 text-[--textSecondary] text-sm'>
+                  186 colaboradores
+                </span>
+              </div>
+              <div className='flex justify-between items-center pr-1 w-full h-5'>
+                <div className='flex items-center gap-2 h-full'>
+                  <span className='bg-[--chartOrange] rounded-full w-1 h-full'></span>
+                  <span className='font-medium leading-none'>21,19%</span>
+                </div>
+                <span className='opacity-50 text-[--textSecondary] text-sm'>
+                  50 colaboradores
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
 
         {/* <div className='gap-3 grid grid-cols-2 sm:grid-cols-3'>
           <div className='group relative flex items-center gap-3 bg-[--backgroundSecondary] hover:shadow-lg p-6 py-10 border border-[--outlinePrimary] hover:border-amber-400/50 rounded-xl w-full overflow-hidden hover:scale-[1.015] transition-all duration-300 cursor-pointer'>
---
+++ components/Chart/Small/Issues/index.tsx
@@ -0,0 +1,93 @@
+'use client'
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+} from 'recharts'
+import { CardContent } from '@/components/ui/card'
+import {
+  type ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from '@/components/ui/chart'
+
+export const description = 'A bar chart with a custom label'
+
+const chartData = [
+  { equipment: 'Equipamentos', withdrawn: 186, notwithdrawn: 50 },
+]
+
+const chartConfig = {
+  withdrawn: {
+    label: 'Regular',
+    color: 'var(--textPrimary)',
+  },
+  notwithdrawn: {
+    label: 'Pendente',
+    color: 'var(--textPrimary)',
+  },
+  label: {
+    color: 'var(--background)',
+  },
+} satisfies ChartConfig
+
+export function SmallChartIssues() {
+  return (
+    <ResponsiveContainer width='100%' height={60}>
+      <CardContent className='p-0 w-full h-full'>
+        <ChartContainer
+          config={chartConfig}
+          className='w-full'
+          style={{ height: `${Math.max(chartData.length * 32, 60)}px` }}
+        >
+          <BarChart
+            data={chartData}
+            layout='vertical'
+            barCategoryGap={2}
+            barSize={18}
+          >
+            <CartesianGrid horizontal={false} />
+            <YAxis
+              dataKey='equipment'
+              type='category'
+              tickLine={false}
+              tickMargin={10}
+              axisLine={false}
+              tickFormatter={value => value.slice(0, 3)}
+              hide
+            />
+            <XAxis type='number' hide />
+            <ChartTooltip
+              cursor={false}
+              content={
+                <ChartTooltipContent
+                  indicator='line'
+                  className='shadow-xl border-[--border]'
+                  labelKey='equipment'
+                  labelFormatter={label => label}
+                />
+              }
+            />
+
+            <Bar
+              dataKey='withdrawn'
+              stackId='a'
+              fill='var(--primaryColor)'
+              radius={[4, 0, 0, 4]}
+            />
+            <Bar
+              dataKey='notwithdrawn'
+              stackId='a'
+              fill='var(--chartOrange)'
+              radius={[0, 4, 4, 0]}
+            />
+          </BarChart>
+        </ChartContainer>
+      </CardContent>
+    </ResponsiveContainer>
+  )
+}
