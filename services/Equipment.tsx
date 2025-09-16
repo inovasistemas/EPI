5f2be87dce2132645afc32e2d9f6ee6037161595
João <contato@joaofelipegomes.com>
2025-09-16 08:58:28 -0300

    equipment services

--- services/Equipment.tsx
+++ services/Equipment.tsx
@@ -59,3 +59,171 @@ export async function getEquipment({ loading, id }: EquipmentService) {
     return null
   }
 }
+
+export async function updateEquipment({
+  loading,
+  id,
+  additional_code,
+  approval_certification,
+  category,
+  composition,
+  cost,
+  details,
+  dimensions,
+  disposable,
+  ean,
+  expiration_date,
+  family,
+  manufacturer,
+  measure,
+  name,
+  picture,
+  price,
+  stock,
+  stock_control,
+  stock_location,
+  stock_maximum,
+  stock_minimum,
+  weight,
+  weight_measure,
+}: UpdateEquipmentService) {
+  try {
+    loading(true)
+    const data: EquipmentServiceData = {}
+
+    if (additional_code) data.additional_code = additional_code
+    if (approval_certification)
+      data.approval_certification = approval_certification
+    if (category) data.category = category
+    if (composition) data.composition = composition
+    if (cost) data.cost = cost
+    if (details) data.details = details
+    if (dimensions) data.dimensions = dimensions
+    if (disposable) data.disposable = disposable
+    if (ean) data.ean = ean
+    if (expiration_date) data.expiration_date = expiration_date
+    if (family) data.family = family
+    if (manufacturer) data.manufacturer = manufacturer
+    if (measure) data.measure = measure
+    if (name) data.name = name
+    if (picture) data.picture = picture
+    if (price) data.price = price
+    if (stock) data.stock = stock
+    if (stock_control) data.stock_control = stock_control
+    if (stock_location) data.stock_location = stock_location
+    if (stock_maximum) data.stock_maximum = stock_maximum
+    if (stock_minimum) data.stock_minimum = stock_minimum
+    if (weight) data.weight = weight
+    if (weight_measure) data.weight_measure = weight_measure
+
+    const response = await axios.put(
+      `${process.env.NEXT_PUBLIC_API_HOST}/equipments/${id}`,
+      data,
+      { withCredentials: true }
+    )
+
+    loading(false)
+    return response
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error)) {
+      return error.response || null
+    }
+
+    loading(false)
+    return null
+  }
+}
+
+export async function createEquipment({
+  loading,
+  additional_code,
+  approval_certification,
+  category,
+  composition,
+  cost,
+  details,
+  dimensions,
+  disposable,
+  ean,
+  expiration_date,
+  family,
+  manufacturer,
+  measure,
+  name,
+  picture,
+  price,
+  stock,
+  stock_control,
+  stock_location,
+  stock_maximum,
+  stock_minimum,
+  weight,
+  weight_measure,
+}: CreateEquipmentService) {
+  try {
+    loading(true)
+    const data: EquipmentServiceData = {}
+
+    if (additional_code) data.additional_code = additional_code
+    if (approval_certification)
+      data.approval_certification = approval_certification
+    if (category) data.category = category
+    if (composition) data.composition = composition
+    if (cost) data.cost = cost
+    if (details) data.details = details
+    if (dimensions) data.dimensions = dimensions
+    if (disposable) data.disposable = disposable
+    if (ean) data.ean = ean
+    if (expiration_date) data.expiration_date = expiration_date
+    if (family) data.family = family
+    if (manufacturer) data.manufacturer = manufacturer
+    if (measure) data.measure = measure
+    if (name) data.name = name
+    if (picture) data.picture = picture
+    if (price) data.price = price
+    if (stock) data.stock = stock
+    if (stock_control) data.stock_control = stock_control
+    if (stock_location) data.stock_location = stock_location
+    if (stock_maximum) data.stock_maximum = stock_maximum
+    if (stock_minimum) data.stock_minimum = stock_minimum
+    if (weight) data.weight = weight
+    if (weight_measure) data.weight_measure = weight_measure
+
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_HOST}/equipments`,
+      data,
+      { withCredentials: true }
+    )
+
+    loading(false)
+    return response
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error)) {
+      return error.response || null
+    }
+
+    loading(false)
+    return null
+  }
+}
+
+export async function deleteEquipment({ loading, id }: EquipmentService) {
+  try {
+    loading(true)
+
+    const response = await axios.delete(
+      `${process.env.NEXT_PUBLIC_API_HOST}/equipments/${id}`,
+      { withCredentials: true }
+    )
+
+    loading(false)
+    return response
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error)) {
+      return error.response || null
+    }
+
+    loading(false)
+    return null
+  }
+}
--- services/types/Equipment.tsx
+++ services/types/Equipment.tsx
@@ -12,3 +12,38 @@ type EquipmentService = {
   loading: React.Dispatch<React.SetStateAction<boolean>>
   id: string
 }
+
+type EquipmentServiceData = {
+  additional_code?: string
+  approval_certification?: string
+  category?: string
+  composition?: string
+  cost?: number
+  details?: string
+  dimensions?: string
+  disposable?: boolean
+  ean?: string
+  expiration_date?: string
+  family?: string
+  manufacturer?: string
+  measure?: string
+  name?: string
+  picture?: string
+  price?: number
+  stock?: number
+  stock_control?: boolean
+  stock_location?: string
+  stock_maximum?: string
+  stock_minimum?: string
+  weight?: string
+  weight_measure?: string
+}
+
+type UpdateEquipmentService = EquipmentServiceData & {
+  loading: React.Dispatch<React.SetStateAction<boolean>>
+  id: string
+}
+
+type CreateEquipmentService = EquipmentServiceData & {
+  loading: React.Dispatch<React.SetStateAction<boolean>>
+}
