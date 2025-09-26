5e18847f1e7c4b40b14e46e52750e7af250c36ca
João <contato@joaofelipegomes.com>
2025-09-26 17:44:41 -0300

    favicon

--- app/entrar/page.tsx
+++ app/entrar/page.tsx
@@ -1,7 +1,7 @@
 import Login from '@/components/Template/Login'
 import { pageMetadata } from '@/lib/misc/metadata'
 
-export const metadata = pageMetadata('Entrar')
+export const metadata = pageMetadata('Entrar', true)
 
 export default function ProductsPage() {
   return <Login />
