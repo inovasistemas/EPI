6e00573830bc609bbb562d3759c6398d4fac15eb
João <contato@joaofelipegomes.com>
2025-10-01 12:50:11 -0300

    re:manufacturer modal skeleton

--- components/Features/Manufacturer/index.tsx
+++ components/Features/Manufacturer/index.tsx
@@ -77,6 +77,7 @@ export function Manufacturer() {
   const handleDeleteManufacturer = async () => {
     const response = await deleteManufacturer({
       id: selectedManufacturer || '',
+      loading: setLoading
     })
 
     if (response) {
